261fc93c2623f9d6664328f881e31cee36fe675d
张鹏 <real_max@zhangpengdeMacBook-Pro.local>
2019-08-26 14:37:13 +0800

    update packjson

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import _ from 'lodash';
 import { cube } from './math.js';
 import printMe from './print.js';
 import './styles.css';
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 function component() {
    var element = document.createElement('pre');
   var btn = document.createElement('button');
